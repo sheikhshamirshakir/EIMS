bff7a8b124a3e1208360cef4c420653af8354ed0
sheikhshamirshakir <sheikhshamirshakir@gmail.com>
2015-03-02 21:00:23 +0600

    test commit

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,7 @@ import views.html.*;
 public class Application extends Controller {
   
     public static Result index() {
-        return ok(index.render("Educational Institute Management System"));
+        return ok(index.render("Educational Ins Management System."));
     }
   
 }
