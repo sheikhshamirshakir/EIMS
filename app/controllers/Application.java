be88845da9712c78d43549f808262fe0208c2319
ranabd <masud.r.rana@gmail.com>
2015-03-02 21:26:30 +0600

    Again Test

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,7 @@ import views.html.*;
 public class Application extends Controller {
   
     public static Result index() {
-        return ok(index.render("Educational Management System.."));
+        return ok(index.render("Educational Management System"));
     }
   
 }
