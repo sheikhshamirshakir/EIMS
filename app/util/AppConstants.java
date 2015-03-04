1207fd9b73305e2206c0aa24e5d990445aa8da81
sheikhshamirshakir <sheikhshamirshakir@gmail.com>
2015-03-04 20:09:39 +0600

    appconstant class

--- app/util/AppConstants.java
+++ app/util/AppConstants.java
@@ -2,5 +2,6 @@ package util;
 
 public class AppConstants {
 
-	public final String ERROR_MESSAGE_ID_NOT_FOUND = "This Id does not exist."; 
+	public static final String ERROR_MESSAGE_ID_NOT_FOUND = "This Id does not exist."; 
+	public static final String SUCCESS_MESSAGE = "Entry successful."; 
 }
