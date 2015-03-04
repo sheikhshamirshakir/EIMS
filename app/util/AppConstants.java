feaef7f8c6f509222c32070e499ef83b56667c68
sheikhshamirshakir <sheikhshamirshakir@gmail.com>
2015-03-04 20:49:27 +0600

    util

--- app/util/AppConstants.java
+++
@@ -1,8 +0,0 @@
-package util;
-
-public class AppConstants {
-
-	public static final String ERROR_MESSAGE_ID_NOT_FOUND = "This Id does not exist."; 
-	public static final String SUCCESS_MESSAGE = "Entry successful."; 
-	public static final String SUCCESSFUL_DELETE_MESSAGE= "Deleted Successfully.";
-}
